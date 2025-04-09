2f1116126ff9593f3ffbf5401a1b775d653caa5f
Semitsi <emrehac6@gmail.com>
2025-04-09 15:13:49 +0300

    Tiledmap lisätty
    Lieto saa laittaa sen, et se on siin ku painaa play

---
+++ Assets/Tiled/Tilemaps/muta.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="muta" tilewidth="128" tileheight="128" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <tile id="9">
+  <image width="128" height="128" source="../../Art/mud1.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="128" source="../../Art/mud2.png"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="128" source="../../Art/mud5.png"/>
+ </tile>
+ <tile id="12">
+  <image width="128" height="128" source="../../Art/muta6.png"/>
+ </tile>
+ <tile id="13">
+  <image width="128" height="128" source="../../Art/muta7.png"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="128" source="../../Art/mutaa.png"/>
+ </tile>
+</tileset>
