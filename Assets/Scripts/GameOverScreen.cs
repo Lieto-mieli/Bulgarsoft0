0a227dc7046bd0f653016d399798552c7ef90021
IC4RU5-D0WN <uyarozgurcan@gmail.com>
2025-05-12 10:00:27 +0300

    Emt unity trollaa

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -1,14 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
+//C
 public class GameOverScreen : MonoBehaviour
 {
     public Text pointsText;
 
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("MENE POIS");
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene("InGame");
+    }
     public void setup(int score)
     {
 
     }
 }
+//C
